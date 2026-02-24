32b11b33441b98e4fb3815719edd9e03cf90ba43
vedanshmishra467-coder <vedanshmishra467@gmail.com>
2026-02-24 09:33:48 +0530

    second commit

---
+++ OOPSBannerApp05.java
@@ -0,0 +1,18 @@
+public class OOPSBannerApp05 {
+    public static void main(String[] args) {
+
+        String[] lines = {
+            String.join(" ", "  *  ", "  *  ", "**  ", " ** "),
+            String.join(" ", "*   *", "*   *", "*  *", "**    "),
+            String.join(" ", "*   *", "*   *", "*  *", "**    "),
+            String.join(" ", "*   *", "*   *", "**  ", " *  "),
+            String.join(" ", "*   *", "*   *", "*    ", "   * "),
+            String.join(" ", "*   *", "*   *", "*    ", "    *"),
+            String.join(" ", "  *  ", "  *  ", "*    ", " *** ")
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
